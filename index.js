6932447fd65d2e72d059852ec6294d5b9d1883da
Victor Kemboi <vickemtoo97@gmail.com>
2024-03-28 14:31:06 +0300

    working on destructuring

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
+
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ');
+const [red,orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [, , , , , indg] = colors;
+const {muppetName, color, song, job, partner} = muppet;
+const {nestedJob, nestedPartner} = nestedMuppet;
+const {song2,song4} = nestedMuppet.album.theMuppetMovie;
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
